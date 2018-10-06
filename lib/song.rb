0696758758dbb0a0a26ac01be894c115e001e599
Christina Williams <christinaxtwilliams@gmail.com>
2018-10-06 00:39:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,3 +6,12 @@ require 'pry'
 	  @@count = 0
 	  @@genres = []
 	  @@artists = []
+ 
+ def initialize(song_name, artist, genre)
+	    @name = song_name
+	    @artist = artist
+	    @genre = genre
+	    @@count += 1
+	    @@genres << genre
+	    @@artists << artist
+end
